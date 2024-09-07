277b7269d6d78764b77c0411a1cb7686124f67ee
prasadk <prasadkatepk@gmail.com>
2024-09-07 16:20:06 +0530

    feat: react-router-dom setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
-  return <>Root page</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* fallback page */}
+        <Route path="*" element={<>Page not found...</>} />
+
+        {/* routes start */}
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
